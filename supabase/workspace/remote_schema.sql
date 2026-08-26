95d2a233a34c2420611f6c0b6afc2840cfdacfd3
nanli <linanenv@gmail.com>
2026-08-27 02:33:32 +0800

    chore: refresh flow eligibility schema workspace

--- supabase/workspace/remote_schema.sql
+++ supabase/workspace/remote_schema.sql
@@ -65984,6 +65984,10 @@ CREATE INDEX "flows_not_emissions_idx" ON "public"."flows" USING "btree" ("state
 
 
 
+CREATE INDEX "flows_portal_embedding_eligible_v1_idx" ON "public"."flows" USING "btree" ("id", "version") WHERE (("state_code" = ANY (ARRAY[100, 200])) AND ("embedding_ft" IS NOT NULL));
+
+
+
 CREATE INDEX "flows_public_latest_keys_cover_idx" ON "public"."flows" USING "btree" ("id", "version" DESC, "modified_at" DESC) INCLUDE ("created_at", "team_id") WHERE ("state_code" = 100);
 
 
